40326fddb4beccc727b442516b56517cc820eb0d
tui777 <davidhouvepun0suy@hotmail.com>
2024-05-11 15:47:39 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-function reorderList(head) {
-  if (!head || !head.next) return;
-  let slow = head;
-  let fast = head;
-  while (fast.next && fast.next.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-  let prev = null;
-  let curr = slow.next;
-  slow.next = null;
-  while (curr) {
-    const next = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = next;
-  }
-  let first = head;
-  let second = prev;
-  while (second.next) {
-    const next = first.next;
-    first.next = second;
-    first = next;
-    const next2 = second.next;
-    second.next = first;
-    second = next2;
-  }
-}
+const message = "Hello world!";
+const messageLength = message.length;
+console.log(messageLength); // 13
