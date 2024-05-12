7df648f386045e7717f156cc5fe3c564ea5a9640
tui777 <davidhouvepun0suy@hotmail.com>
2024-05-12 21:06:36 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const message = "Hello world!";
-const messageLength = message.length;
-console.log(messageLength); // 13
+// Fund Alice and sponsor accounts
+await aptos.fundAccount({
+  accountAddress: aliceAddress,
+  amount: ALICE_INITIAL_BALANCE,
+});
+await aptos.fundAccount({
+  accountAddress: sponsorAddress,
+  amount: SPONSOR_INITIAL_BALANCE,
+});
+
+// Show account balances
+const aliceBalanceBefore = await aptos.getAccountCoinsData({
+  accountAddress: aliceAddress,
+});
+const sponsorBalanceBefore = await aptos.getAccountCoinsData({
+  accountAddress: sponsorAddress,
+});
